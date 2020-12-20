c36d1ccaa781500e11b3cf2ddb6d25a3d92b1d59
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-20 18:25:04 -0300

    Estrutura inicial do menu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,44 @@ namespace Revisão
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular vmédia geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+
+            while (opcaoUsuario.ToUpper() != "X") {
+
+                switch(opcaoUsuario){
+
+                    case "1":
+                        //TODO: adicionar aluno
+                        break;
+
+                    case "2":
+                        //TODO: listar alunos
+                        break;
+
+                    case "3":
+                        //TODO: calcular média geral
+                        break;
+                    
+                    default:
+                        throw new ArgumentOutOfRangeException;
+                }
+
+                Console.WriteLine("Informe a opção desejada: ");
+                Console.WriteLine("1 - Inserir novo aluno");
+                Console.WriteLine("2 - Listar alunos");
+                Console.WriteLine("3 - Calcular vmédia geral");
+                Console.WriteLine("X - Sair");
+                Console.WriteLine();
+
+                opcaoUsuario = Console.ReadLine();
+            }
         }
     }
 }
