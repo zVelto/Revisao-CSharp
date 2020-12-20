eb8d23de7f735f61caf120b0b4abc5fb3827a406
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-20 19:40:58 -0300

    Adicionando opção de inserir e listar alunos

--- Program.cs
+++ Program.cs
@@ -6,44 +6,68 @@ namespace Revisão
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Informe a opção desejada: ");
-            Console.WriteLine("1 - Inserir novo aluno");
-            Console.WriteLine("2 - Listar alunos");
-            Console.WriteLine("3 - Calcular vmédia geral");
-            Console.WriteLine("X - Sair");
-            Console.WriteLine();
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
+            string opcaoUsuario = obterOpcaoUsuario();
 
-            string opcaoUsuario = Console.ReadLine();
+            while (opcaoUsuario.ToUpper() != "X")
+            {
 
-            while (opcaoUsuario.ToUpper() != "X") {
-
-                switch(opcaoUsuario){
+                switch (opcaoUsuario)
+                {
 
                     case "1":
-                        //TODO: adicionar aluno
+                        Console.WriteLine("Informe o nome do aluno:");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno:");
+
+                        if(decimal.TryParse(Console.ReadLine(), out decimal nota)){
+                            aluno.Nota = nota;
+                        }
+                        else{
+                            throw new ArgumentException("O valor da nota deve ser decimal");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+                        
                         break;
 
                     case "2":
-                        //TODO: listar alunos
+                        foreach(var a in alunos){
+                            if(!string.IsNullOrEmpty(a.Nome)) {
+                                Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota}");
+                            }
+                        }
                         break;
 
                     case "3":
                         //TODO: calcular média geral
                         break;
-                    
+
                     default:
-                        throw new ArgumentOutOfRangeException;
+                        throw new ArgumentOutOfRangeException();
                 }
 
-                Console.WriteLine("Informe a opção desejada: ");
-                Console.WriteLine("1 - Inserir novo aluno");
-                Console.WriteLine("2 - Listar alunos");
-                Console.WriteLine("3 - Calcular vmédia geral");
-                Console.WriteLine("X - Sair");
-                Console.WriteLine();
-
-                opcaoUsuario = Console.ReadLine();
+                opcaoUsuario = obterOpcaoUsuario();
             }
+        }
+
+        private static string obterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
