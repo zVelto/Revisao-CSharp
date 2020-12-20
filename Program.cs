0dfb3a731efbbb382a190925b978bd15263c30b1
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-20 18:12:01 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Revisão
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
